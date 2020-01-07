12a3b4bf6651d2ab0756dd6f9344a0dcbe988e2b
irailtonreis <irailton.oliveirap1@gmail.com>
2020-01-07 09:23:53 -0300

    Adicionado debounce na animacao-scroll

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,9 +1,10 @@
+import debounce from './debounce.js';
 
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
   // Pega a distancia de cada item em
@@ -16,7 +17,6 @@ export default class ScrollAnima {
         offset: Math.floor(offset - this.windowMetade),
       };
     });
-    console.log(this.distance);
   }
 
   // Verifica a distância de cada objeto
